2a8e30e1be13d15a94a31551588aa3b16ca354da
liubo404 <liubo404@gmail.com>
2016-01-12 17:51:24 +0800

    discover meteor

--- meteor/LendLib/LendLib.js
+++ meteor/LendLib/LendLib.js
@@ -1,7 +1,11 @@
+lists = new Mongo.Collection("lists");
+
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
-
+    Session.setDefault('counter', 0);
+    Session.set('adding_category', false);
+    
+/*
   Template.hello.helpers({
     counter: function () {
       return Session.get('counter');
@@ -14,6 +18,41 @@ if (Meteor.isClient) {
       Session.set('counter', Session.get('counter') + 1);
     }
   });
+*/
+    Template.categories.helpers({
+	lists: function(){
+	    return lists.find({},{sort: {Category:1}});
+	},
+	new_cat: function(){
+	    return Session.equals('adding_category', true);
+	}
+    });
+
+    Template.categories.events({
+	'click #btnNewCat': function(e,t){
+	    Session.set('adding_category', true);
+	    Tracker.flush();
+	    focusText(t.find("#add-category"));
+	},
+	'keyup #add-category': function (e,t){
+	    if (e.which === 13)
+	    {
+		var catVal = String(e.target.value || "");
+		if (catVal){
+		    lists.insert({Category:catVal});
+		    Session.set('adding_category', false);
+		}
+	    }
+	},
+	'focusout #add-category': function(e,t){
+	    Session.set('adding_category',false);
+	}
+    });
+
+    function focusText(i){
+	i.focus();
+	i.select();
+    };
 }
 
 if (Meteor.isServer) {
---
+++ meteor/microscope/client/templates/posts/posts_list.js
@@ -0,0 +1,14 @@
+var postsData = [
+    {
+	title: 'Introducing Telescope',
+	url: 'http://www.google.com'
+    },
+    {
+	title: 'Meteor',
+	url: 'http://meteor.com'
+    }
+];
+
+Template.postsList.helpers({
+    posts: postsData
+});
